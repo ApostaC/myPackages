172b5a6a1f5cba1e6c7c0058779542684c56f2ab
ApostaC <Aposta@pku.edu.cn>
2017-09-14 19:44:11 +0800

    add help

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
-
+help(){
+	cat<< HELP
+this is the script to manipulate HPL.dat 
+USAGE : ./run.sh [-aghd] [params]
+	-d	: run in default settings; 
+		  first getData() then analysis()
+	-a	: call function analysis()
+	-g num	: call function getData()
+	-h	: call fuction help()
+HELP
+	exit 0
+}
 getData(){
 	echo "Input the directory of Data folder :"
 	read dataDir
@@ -8,7 +19,7 @@ getData(){
 		echo "COPY ${file} to ./HPL.dat"
 		cp -f ${file} ./HPL.dat
 		echo "starting xhpl"
-		mpirun -np 2 ./xhpl
+		mpirun -np $1 ./xhpl
 	done
 }
 
@@ -20,5 +31,20 @@ analysis(){
 	done
 }
 
+if [ $# -lt 1 ]; then
+	help
+fi
+
+while [ -n "$1" ]; do 
+case $1 in
+	-h) help;shift 1;;
+	-g) shift 1;getData $1;;
+	-a) analysis;;
+	-d) shift 1;getData $1;analysis;;
+	-*) echo "Unknown command $1, use ./run.sh -h for help";exit 1;;
+	*) break;;
+esac
+done
+
 #getData
-analysis
+#analysis
